0573bf0edd78e7aed1f0f7df858910c9103b8909
unknown <cookieKang@gmail.com>
2019-09-05 02:41:43 +0900

    3.26

--- TodoApp/app/App.js
+++ TodoApp/app/App.js
@@ -287,6 +287,88 @@ import TodoList from './TodoList'
 
 //3.17 - 3.19 
 
+// let todoIndex = 0
+
+// class App extends Component {
+
+//     constructor() {
+// 		super()
+// 		this.state = {
+// 			inputValue: '',
+// 			todos: [],
+// 			type: 'All'
+//         }
+//         this.submitTodo = this.submitTodo.bind(this) 
+//         this.toggleComplete = this.toggleComplete.bind(this)  
+//         this.deleteTodo = this.deleteTodo.bind(this) 
+
+//     }
+    
+//     inputChange(inputValue) { 
+//         console.log(' Input Value: ' , inputValue) 
+//         this.setState({ inputValue }) 
+//     }
+
+//     submitTodo () {   
+//         if (this.state.inputValue.match(/^\s*$/)) {
+//           return
+//         }
+//         const todo = {
+//           title: this.state.inputValue,
+//           todoIndex,
+//           complete: false
+//         }
+//         todoIndex++ 
+//         const todos = [...this.state.todos, todo] 
+//         this.setState({ todos, inputValue: '' }, () => { 
+//             console.log('State: ', this.state) 
+//         })
+//     }
+
+//     deleteTodo (todoIndex) { 
+//         let { todos } = this.state
+//         todos = todos.filter((todo) => todo.todoIndex !== todoIndex)
+//         this.setState({ todos })
+//     }
+//     toggleComplete (todoIndex) {
+//         let todos = this.state.todos
+//         todos.forEach((todo) => {
+//             if (todo.todoIndex === todoIndex) {
+//                 todo.complete = !todo.complete
+//             }
+//         })
+//         this.setState({ todos })
+//     }
+
+    
+//     render() {
+
+//         const { inputValue, todos } = this.state
+        
+//         return (
+//             <View style={styles.container}>
+//                 <ScrollView
+//                     keyboardShouldPersistTaps='always'
+//                     style={styles.content}>
+//                     <Heading />
+//                     <Input inputValue={inputValue} inputChange={(text) => this.
+//                 inputChange(text)} />
+//                     <TodoList 
+//                      toggleComplete={this.toggleComplete}
+//                      deleteTodo={this.deleteTodo}
+//                      todos={todos}          
+//                     />
+//                     <Button submitTodo={this.submitTodo} />
+//                 </ScrollView>
+//             </View>
+//         )
+//     }
+// }
+
+//3.22 , 3.25 
+
+import TabBar from './TabBar'
+
 let todoIndex = 0
 
 class App extends Component {
@@ -301,6 +383,7 @@ class App extends Component {
         this.submitTodo = this.submitTodo.bind(this) 
         this.toggleComplete = this.toggleComplete.bind(this)  
         this.deleteTodo = this.deleteTodo.bind(this) 
+        this.setType = this.setType.bind(this)
 
     }
     
@@ -330,6 +413,7 @@ class App extends Component {
         todos = todos.filter((todo) => todo.todoIndex !== todoIndex)
         this.setState({ todos })
     }
+
     toggleComplete (todoIndex) {
         let todos = this.state.todos
         todos.forEach((todo) => {
@@ -340,10 +424,14 @@ class App extends Component {
         this.setState({ todos })
     }
 
+    setType (type) {
+        this.setState({ type })
+    }
+    
     
     render() {
 
-        const { inputValue, todos } = this.state
+        const { inputValue, todos, type } = this.state
         
         return (
             <View style={styles.container}>
@@ -354,16 +442,20 @@ class App extends Component {
                     <Input inputValue={inputValue} inputChange={(text) => this.
                 inputChange(text)} />
                     <TodoList 
+                     type={type}
                      toggleComplete={this.toggleComplete}
                      deleteTodo={this.deleteTodo}
                      todos={todos}          
                     />
                     <Button submitTodo={this.submitTodo} />
+                    
                 </ScrollView>
+                <TabBar type={type} setType={this.setType} />
             </View>
         )
     }
 }
+
 
 const styles = StyleSheet.create({
 	container: {
--- TodoApp/app/TodoList.js
+++ TodoApp/app/TodoList.js
@@ -20,7 +20,40 @@ import Todo from './Todo'
 //3.20
 
 
-const TodoList = ({ todos, deleteTodo, toggleComplete }) => {
+// const TodoList = ({ todos, deleteTodo, toggleComplete }) => {
+// 	todos = todos.map((todo, i) => {
+// 		return (
+// 			<Todo
+// 				deleteTodo={deleteTodo}
+// 				toggleComplete={toggleComplete}
+// 				key={i}
+// 				todo={todo} />
+// 			)
+//     })
+
+//     return (
+// 		<View>
+// 			{todos}
+// 		</View>
+// 	)
+// }
+
+//3.26
+
+const TodoList = ({ todos, deleteTodo, toggleComplete, type }) => {
+
+    const getVisibleTodos = (todos, type) => {
+		switch (type) {
+			case 'All':
+				return todos
+			case 'Complete':
+				return todos.filter((t) => t.complete)
+			case 'Active':
+				return todos.filter((t) => !t.complete)
+		}
+	}
+	todos = getVisibleTodos(todos, type)
+
 	todos = todos.map((todo, i) => {
 		return (
 			<Todo
