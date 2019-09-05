9a488099313547850edcff0add1ebab109618223
unknown <cookieKang@gmail.com>
2019-09-05 14:44:04 +0900

    5.1

---
+++ ch05/App.js
@@ -0,0 +1,129 @@
+//5.1 
+
+import React, { Component } from 'react';
+import { Image, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.cardContainer}>
+          <View style={styles.cardImageContainer}>
+            <Image style={styles.cardImage} source={require('./user.png')}/>
+          </View>
+          <View>
+            <Text style={styles.cardTitle}>
+              John Doe
+            </Text>
+          </View>
+          <View style={styles.cardSubtitleContainer}>
+            <Text style={styles.cardSubtitle}>
+              React Native Developer
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.cardDescription}>
+              John is a really great JavaScript developer. He
+              loves using JS to build React Native applications
+              for iOS and Android.
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const profileCardColor = 'dodgerblue';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cardContainer: {
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 20,
+    backgroundColor: profileCardColor,
+    width: 300,
+    height: 400,
+    ...Platform.select({ //플랫폼에 따라 카드 컨테이너에 음영 넣기
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        elevation: 15
+      }
+    })
+
+  },
+  cardImageContainer: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginTop: 30,
+    paddingTop: 15,
+    ...Platform.select({ //원형 이미지에 음영 넣기
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10,
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        borderWidth: 3,
+        borderColor: 'black',
+        elevation: 15
+      }
+    })
+
+  },
+  cardImage: {
+    width: 80,
+    height: 80
+  },
+  cardTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginTop: 30,
+    textShadowColor: 'black', // 음영 색상 지정
+    textShadowOffset: {  //음영의 offset 지정 오른쪽 하단으로 음영
+      height: 2,
+      width: 2
+    },
+    textShadowRadius: 3 //음영의 반경 
+  },
+  cardSubtitleContainer: {
+    borderColor: 'black',
+    borderWidth: 3,
+    borderTopWidth: 0,
+    borderRightWidth: 0,
+    borderLeftWidth: 0,
+  },
+  cardSubtitle: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  cardDescription: {
+    fontStyle: 'italic',
+    marginTop: 10,
+    marginRight: 40,
+    marginLeft: 40,
+    marginBottom: 10
+  }
+});
